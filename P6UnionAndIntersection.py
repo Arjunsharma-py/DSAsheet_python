4105230f937b17c67ac3a42d7b55d80a892b9735
Arjun Sharma <arjunsharma5330@gmail.com>
2022-08-24 00:26:52 +0530

    for testing purpose

---
+++ P6UnionAndIntersection.py
@@ -0,0 +1,12 @@
+def union_arr(list1, list2):
+    i, j = 0, 0
+    c = []
+    for k in range(len(list1) + len(list2)):
+        if list1[]
+        if list1[i] < list2[j] and i != len(list1):
+            c.append(list[i])
+            i += 1
+        elif list1[i] > list2[j]:
+            c.append(list[j])
+            j += 1
+        
